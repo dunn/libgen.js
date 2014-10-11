3c0f55d114542e6a9c5cdab5811e05cb4a4d2a41
Alex Dunn <dunn.alex@gmail.com>
2014-10-10 19:13:09 -0700

    test: make sure latest id # is plausibly hihg

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
 var assert = require('assert');
 
 describe('latest.id()', function(){
-  it('should return number', function(done){
+  it('should return a number over 1282650', function(done){
     var latest = require('../lib/latest.js');
     latest.id('http://gen.lib.rus.ec',function(err, data){
       if (err) return done(err);
       if (!parseInt(data)) {
         return done(new Error('Returned a NaN'));
+      }
+      if (parseInt(data) < 1282650) {
+        return done(new Error('Number returned (' +
+                              data + ') is too low'));
       }
       return done(null,data);
     });
