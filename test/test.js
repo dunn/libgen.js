c8a298c51298f65090734b77bbe5f1df86a7b3e7
Alex Dunn <dunn.alex@gmail.com>
2014-10-10 19:45:08 -0700

    mirror picker
    - with tests!
    - longer timeout cause yikers

--- test/test.js
+++ test/test.js
@@ -1,9 +1,26 @@
 var assert = require('assert');
 
+// get a working mirror and use that for the rest of the tests
+var mirror;
+describe('mirrors.js', function(){
+  it('should return a working mirror base URL', function(done){
+    var getMirror = require('../lib/mirrors.js');
+    getMirror(function(err,string){
+      if (err) return done(err);
+      if (!string){
+        return done(new Error('somehow returned an empty string'));
+      }
+      mirror = string;
+      return done();
+    });
+  });
+});
+
+
 describe('latest.id()', function(){
   it('should return a number over 1282650', function(done){
     var latest = require('../lib/latest.js');
-    latest.id('http://gen.lib.rus.ec',function(err, data){
+    latest.id(mirror,function(err, data){
       if (err) return done(err);
       if (!parseInt(data)) {
         return done(new Error('Returned a NaN'));
