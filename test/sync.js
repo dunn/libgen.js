3acf10a7cc4245aa029007aa1148dd4e35f74849
Alex Dunn <dunn.alex@gmail.com>
2014-10-19 16:23:31 -0700

    new function clean()
    - removes texts (JSON objects) that are missing any of the specified fields
    
    - new tests, and renamed old asynchronous tests

---
+++ test/sync.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+
+var clean = require('../lib/clean.js');
+var single = require('./single.json');
+var multiple = require('./multiple.json');
+describe('clean()',function(){
+  describe('single text',function(){
+    describe('strip if no Publisher (string and array)',function(){
+      it('should remove the object and return nothing',function(){
+        var empty = !clean(single,'Publisher');
+        assert.ok(empty);
+      });
+      it('should remove the object and return nothing',function(){
+        var empty = !clean(single,['Publisher']);
+        assert.ok(empty);
+      });
+    });
+    describe('strip if no Title (string and array)',function(){
+      it('should return the object',function(){
+        var json = clean(single,'Title');
+        assert.equal(typeof json,'object');
+      });
+      it('should return the object',function(){
+        var json = clean(single,['Title']);
+        assert.equal(typeof json,'object');
+      });
+    });
+  });
+  describe('multiple texts',function(){
+    describe('strip if not Cleaned (string and array)',function(){
+      it('should return nothing',function(){
+        var empty = !clean(multiple,'Cleaned');
+        assert.ok(empty);
+      });
+      it('should return nothing',function(){
+        var empty = !clean(multiple,['Cleaned']);
+        assert.ok(empty);
+      });
+    });
+    describe('strip if not Searchable (string and array)',function(){
+      it('should return an array of two texts',function(){
+        var json = clean(multiple,'Searchable');
+        assert.equal(json.length,2);
+      });
+      it('should return an array of two texts',function(){
+        var json = clean(multiple,['Searchable']);
+        assert.equal(json.length,2);
+      });
+    });
+  });
+});
