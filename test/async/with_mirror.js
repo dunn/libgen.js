66cbef3d7581b6c2cedef7f8d1a87370f2b720d4
Alex Dunn <dunn.alex@gmail.com>
2014-12-06 13:36:36 -0800

    simplify random.js tests

--- test/async/with_mirror.js
+++ test/async/with_mirror.js
@@ -1,10 +1,15 @@
 var assert = require('assert');
 
+var latest = require('../../lib/latest.js');
+var getMirror = require('../../lib/mirror.js');
+var random = require('../../lib/random.js');
+var check = require('../../lib/check.js');
+var search = require('../../lib/search.js');
+
 // get a working mirror and use that for the rest of the tests
 var mirror;
 describe('mirrors.js', function(){
   it('should return a working mirror base URL', function(done){
-    var getMirror = require('../../lib/mirror.js');
     getMirror(function(err,string){
       if (err) return done(err);
       if (!string){
@@ -17,7 +22,6 @@ describe('mirrors.js', function(){
   });
 });
 
-var latest = require('../../lib/latest.js');
 describe('latest.id', function(){
   it('should return a number over 1282650', function(done){
     latest.id(mirror,function(err, data){
@@ -43,7 +47,6 @@ describe('latest.text',function(){
   });
 });
 
-var random = require('../../lib/random.js');
 describe('random.text',function(){
   it('should return at least one JSON object',function(done){
     var opts = {
@@ -55,9 +58,38 @@ describe('random.text',function(){
       return done(null,data);
     });
   });
+  it('should return exactly 30 English PDFs from 2000 with Titles', function(done){
+    var opts = {
+      mirror: mirror,
+      count: 30,
+      fields: [
+        'Title',
+        { Language: 'English' },
+        { Year: '2000',
+          Extension: 'pdf' }
+      ]
+    };
+    random.text(opts,function(err,data){
+      if (err) return done(err);
+      var n = data.length;
+      // console.log('received ' + n + ' texts');
+      assert.equal(n,opts.count,
+                   'received wrong # of texts (' + n + ')');
+      while (n--){
+        assert.ok(check.hasField(data[n],'Title'),
+                 'text ' + n + ' is missing Title');
+        assert.ok(check.hasField(data[n],'Year', '2000'),
+                  'text ' + n + ' has Year ' + data[n].Year);
+        assert.ok(check.hasField(data[n],'Language','English'),
+                 'text ' + n + ' is in ' + data[n].Language);
+        assert.ok(check.hasField(data[n],'Extension','pdf'),
+                 'text ' + n + ' is a ' + data[n].Extension);
+      }
+      return done();
+    });
+  });
 });
 
-var search = require('../../lib/search.js');
 describe('search.js',function(){
   it('should return an array of 30 JSON objects',function(done){
     var options = {
