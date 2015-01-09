d8c7f477514655a605259494d37ba51f080cfa3d
Alex Dunn <dunn.alex@gmail.com>
2015-01-09 13:28:48 -0800

    separate file for listing available mirrors
    should allow easier updating in case of future changes

---
+++ available_mirrors.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    baseUrl: 'http://gen.lib.rus.ec',
+    canDownloadDirect: false
+  },
+  {
+    baseUrl: 'http://libgen.in',
+    canDownloadDirect: true
+  }
+];
--- test/async/check.js
+++ test/async/check.js
@@ -1,9 +1,21 @@
 var assert = require('assert');
 var check = require('../../lib/check.js');
+
+var goodMd5 = 'c95589cd1b9dfbd919b3d1b6a5665673';
+var badMd5 = '8e69614e79fd09ccdc60honkhonk';
+var badJson = require('../json/404.json');
+
 describe('check.js',function(){
   describe('canDownload()',function(){
+    it('should return a url string',function(done){
+      check.canDownload(goodMd5,function(err,response){
+        if (typeof response === 'string') {
+          return done();
+        }
+        return done(new Error('Wrong response'));
+      });
+    });
     it('should return a 404',function(done){
-      var badMd5 = '8e69614e79fd09ccdc60honkhonk';
       check.canDownload(badMd5,function(err,response){
         if (err) {
           return done();
@@ -12,7 +24,6 @@ describe('check.js',function(){
       });
     });
     it('should return a 404',function(done){
-      var badJson = require('../json/404.json');
       check.canDownload(badJson,function(err,response){
         if (err) {
           return done();
